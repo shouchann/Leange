b4d40c7936bf3f973c3566011e2a2e26cf967711
shouchann <shouichi361@gmail.com>
2023-04-17 09:38:07 +0900

    smooth scroll実装

--- style.js
+++ style.js
@@ -1,6 +1,5 @@
-$('#page-link a[href*="#"]').click(function () {//全てのページ内リンクに適用させたい場合はa[href*="#"]のみでもOK
-	var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
-	var pos = $(elmHash).offset().top-70;//idの上部の距離からHeaderの高さを引いた値を取得
-	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
-	return false;
-});
+ // ページ内リンクのみ取得
+ var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 300,//スクロールする速さ
+    header: '#header'//固定ヘッダーがある場合
+   });
